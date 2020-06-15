b11bf0de64e42a8483d71e0223521b8f13c78441
kaxaru <khidechi@gmail.com>
2020-06-15 16:43:41 +0300

    feat: add base authorization with auth0

---
+++ src/Components/authRoute.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { useAuth0 } from './Auth/auth0';
+import Login from './login';
+
+const AuthRoute = ({ component: Component, path, ...rest }) => {
+  const { isAuthenticated } = useAuth0();
+  console.log(isAuthenticated, 'auth')
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return isAuthenticated === true ? <Component {...rest} /> : <Login />;
+};
+
+export default AuthRoute;
--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ import ReactDom from 'react-dom';
 import App from './Components/app';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+import { Auth0Provider } from './Components/Auth/auth0';
+import auth0config from './Components/config';
+
+ReactDom.render(
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Auth0Provider {...auth0config}>
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root'),
+);
