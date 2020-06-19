21fdd8167bc6efd69c738d4db73ad844854aa2a0
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-19 21:14:52 +0300

    feat: add team info

---
+++ src/components/aboutTeam/teamInfo/teamInfo.js
@@ -0,0 +1,80 @@
+const teamInfo = [{
+  name: {
+    first: 'Sergey',
+    sur: ' ',
+    nick: 'SeGeGha',
+  },
+  avatar: 'https://avatars2.githubusercontent.com/u/21230284?s=400&u=8c419c79f7ab1e817ce05f2e1ecacc261f5aeea2&v=4',
+  hrefGitHub: 'https://github.com/segegha',
+  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  id: 1,
+  contribution: '70%',
+}, {
+  name: {
+    first: 'Kanstantsin',
+    sur: ' Kulahin',
+    nick: 'blacktruth',
+  },
+  avatar: 'https://avatars0.githubusercontent.com/u/60391325?s=400&u=a246f4fe7576ce34a6e702e1a32bd606438b8d17&v=4',
+  hrefGitHub: 'https://github.com/blacktruth',
+  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  id: 2,
+  contribution: '100%',
+}, {
+  name: {
+    first: 'Vitaly',
+    sur: ' Raicheu',
+    nick: 'RVitaly1978',
+  },
+  avatar: 'https://avatars1.githubusercontent.com/u/55198465?s=400&v=4',
+  hrefGitHub: 'https://github.com/rvitaly1978',
+  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  id: 3,
+  contribution: '90%',
+}, {
+  name: {
+    first: 'Kaxaru',
+    sur: ' ',
+    nick: 'kaxaru',
+  },
+  avatar: 'https://avatars2.githubusercontent.com/u/6479576?s=400&u=4510e62e237855c67bec435daff8a6d8cfbe2e4a&v=4',
+  hrefGitHub: 'https://github.com/kaxaru',
+  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  id: 4,
+  contribution: '90%',
+}, {
+  name: {
+    first: 'Ali',
+    sur: ' ',
+    nick: 'Sultonali-Ali',
+  },
+  avatar: 'https://avatars2.githubusercontent.com/u/54768203?s=400&u=b84697c9df943929c4739625b1d480309146caa0&v=4',
+  hrefGitHub: 'https://github.com/sultonali-ali',
+  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  id: 5,
+  contribution: '60%',
+}, {
+  name: {
+    first: 'Vitaliy',
+    sur: ' Tomashevich',
+    nick: 'VitaliyTom',
+  },
+  avatar: 'https://avatars2.githubusercontent.com/u/39959686?s=400&u=e24881ac77dfa6c87d99c46b77515f443a89c962&v=4',
+  hrefGitHub: 'https://github.com/VitaliyTom',
+  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  id: 6,
+  contribution: '1%',
+}, {
+  name: {
+    first: 'julia',
+    sur: ' ',
+    nick: 'juliamv',
+  },
+  avatar: '/src/assets/image/avatar/woman.png',
+  hrefGitHub: '#',
+  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  id: 7,
+  contribution: '100%',
+}];
+
+export default teamInfo;
