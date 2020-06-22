b4938220dfa8c407ea537d992d3d292b2f6809ba
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-06-22 15:15:20 +0500

    feat: create Promo page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import App from './app';
+import App from './pages/app/App';
 import 'semantic-ui-css/semantic.min.css';
+import './styles/index.scss';
 
 ReactDom.render(
   <App />,
--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -3,27 +3,30 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
-import History from './utils/history';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import LoginPage from './pages/loginPage/LoginPage';
-import Logout from './pages/logoutPage/Logout';
-import SignupPage from './pages/signupPage/SignupPage';
-import Dictionary from './pages/Dictionary';
-import Statistic from './pages/Statistic';
-import Promo from './pages/Promo';
-import About from './pages/About';
-import Settings from './pages/Settings';
 
-import Header from './components/header/Header';
+import History from '../../utils/history';
+import Home from '../Home';
+import NotFound from '../NotFound';
+import LoginPage from '../loginPage/LoginPage';
+import Logout from '../logoutPage/Logout';
+import SignupPage from '../signupPage/SignupPage';
+import Dictionary from '../Dictionary';
+import Statistic from '../Statistic';
+import Promo from '../promoPage/Promo';
+import About from '../About';
+import Settings from '../Settings';
 
-import './app.scss';
+import Header from '../../components/header/Header';
+import GreetingWrapper from '../../components/greetingWrapper/GreetingWrapper';
+
+import './App.scss';
 
 const App = () => (
   <Router history={History}>
     <div className="app-wrapper">
       <Header />
       <div className="app-main">
+        <GreetingWrapper />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/signin" component={LoginPage} />
