7c6516eda9f2a93b12ce30067712e0e5e53e8b25
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-06-24 03:41:22 +0300

    feat: add formatted error answer on input

--- src/components/wordCard/wordInput/WordInput.js
+++ src/components/wordCard/wordInput/WordInput.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const getFormattedWordOnError = (errorWord, word) => {
+	const correct = word.split('');
+	const error = errorWord.split('');
+	let errors = Math.abs(correct.length - error.length);
+
+	for (let i = 0; i <= correct.length - 1; i += 1) {
+		if (error[i] && correct[i].toLowerCase() !== error[i].toLowerCase()) {
+			errors += 1;
+		}
+	}
+
+	const elementClass = (errors >= 0.5 * correct.length)
+		? 'word__size--errorMax'
+		: 'word__size--errorMidl'
+
+	const formattedLetters = [];
+	for (let i = 0; i <= correct.length - 1; i += 1) {
+		const element = (error[i] && correct[i].toLowerCase() === error[i].toLowerCase())
+			? <span key={i} >{correct[i]}</span>
+			: <span key={i} className={elementClass}>{correct[i]}</span>;
+
+		formattedLetters.push(element);
+	}
+	
+	return formattedLetters;
+}
+
 const WordInput = ({
 	word: { word, wordTranslate },
 	helpSettings: { isTranslateShow },
 	onInputEnter, onInputFocus, onInputChange, isWordInput, inputRef, value, isCorrect, isPrevWord,
 }) => {
+	const currentValue = isWordInput
+		? inputRef.current.value
+		: '';
+
 	let classesTranslate = 'text-item--translate text-item--hidden';
 	if (isWordInput || isPrevWord) {
 		classesTranslate = 'text-item--translate';
@@ -16,14 +47,14 @@ const WordInput = ({
 		classes += ' word__size--correct';
 	}
 
-	if (isWordInput && !isCorrect) {
-		classes += ' word__size--error';
-	}
+	const marked = (isWordInput && !isCorrect)
+		? <span className='word__size word__size--visible'>{getFormattedWordOnError(currentValue, word)}</span>
+		: <span className={classes}>{word}</span>
 
 	const element = (
 		<div className='word__container'>
 			<span className='word__wrapper'>
-				<span className={classes}>{word}</span>
+				{marked}
 				<input className='word__input'
 					type='text'
 					value={value}
