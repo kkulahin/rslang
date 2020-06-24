7aa0766ab41dee7d985a840da4b97ad878c1da1c
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-24 12:05:31 +0300

    refactor: add text 'Lorem Ipsum...' and delete contribution precent

--- src/components/aboutTeam/teamInfo/teamInfo.js
+++ src/components/aboutTeam/teamInfo/teamInfo.js
@@ -6,9 +6,18 @@ const teamInfo = [{
   },
   avatar: 'https://avatars2.githubusercontent.com/u/21230284?s=400&u=8c419c79f7ab1e817ce05f2e1ecacc261f5aeea2&v=4',
   hrefGitHub: 'https://github.com/segegha',
-  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
   id: 1,
-  contribution: '70%',
+  contribution: `Where can I get some? 
+  There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. 
+  If you are going to use a passage of Lorem Ipsum, 
+  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
 }, {
   name: {
     first: 'Kanstantsin',
@@ -17,9 +26,18 @@ const teamInfo = [{
   },
   avatar: 'https://avatars0.githubusercontent.com/u/60391325?s=400&u=a246f4fe7576ce34a6e702e1a32bd606438b8d17&v=4',
   hrefGitHub: 'https://github.com/blacktruth',
-  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged. `,
   id: 2,
-  contribution: '100%',
+  contribution: `Where can I get some? 
+  There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. 
+  If you are going to use a passage of Lorem Ipsum, 
+  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
 }, {
   name: {
     first: 'Vitaly',
@@ -28,9 +46,18 @@ const teamInfo = [{
   },
   avatar: 'https://avatars1.githubusercontent.com/u/55198465?s=400&v=4',
   hrefGitHub: 'https://github.com/rvitaly1978',
-  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
   id: 3,
-  contribution: '90%',
+  contribution: `Where can I get some? 
+  There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. 
+  If you are going to use a passage of Lorem Ipsum, 
+  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
 }, {
   name: {
     first: 'Kaxaru',
@@ -39,9 +66,18 @@ const teamInfo = [{
   },
   avatar: 'https://avatars2.githubusercontent.com/u/6479576?s=400&u=4510e62e237855c67bec435daff8a6d8cfbe2e4a&v=4',
   hrefGitHub: 'https://github.com/kaxaru',
-  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
   id: 4,
-  contribution: '90%',
+  contribution: `Where can I get some? 
+  There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. 
+  If you are going to use a passage of Lorem Ipsum, 
+  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
 }, {
   name: {
     first: 'Ali',
@@ -50,9 +86,18 @@ const teamInfo = [{
   },
   avatar: 'https://avatars2.githubusercontent.com/u/54768203?s=400&u=b84697c9df943929c4739625b1d480309146caa0&v=4',
   hrefGitHub: 'https://github.com/sultonali-ali',
-  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
   id: 5,
-  contribution: '60%',
+  contribution: `Where can I get some? 
+  There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. 
+  If you are going to use a passage of Lorem Ipsum, 
+  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
 }, {
   name: {
     first: 'Vitaliy',
@@ -61,9 +106,18 @@ const teamInfo = [{
   },
   avatar: 'https://avatars2.githubusercontent.com/u/39959686?s=400&u=e24881ac77dfa6c87d99c46b77515f443a89c962&v=4',
   hrefGitHub: 'https://github.com/VitaliyTom',
-  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
   id: 6,
-  contribution: '1%',
+  contribution: `Where can I get some? 
+  There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. 
+  If you are going to use a passage of Lorem Ipsum, 
+  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
 }, {
   name: {
     first: 'julia',
@@ -72,9 +126,18 @@ const teamInfo = [{
   },
   avatar: '/src/assets/image/avatar/woman.png',
   hrefGitHub: '#',
-  aboutPerson: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, 
+  remaining essentially unchanged.`,
   id: 7,
-  contribution: '100%',
+  contribution: `Where can I get some? 
+  There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. 
+  If you are going to use a passage of Lorem Ipsum, 
+  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
 }];
 
 export default teamInfo;
