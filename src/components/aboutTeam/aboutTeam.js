d2bdc1ad9bceb9c8f8caddff56ea50515cb8f99c
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-24 12:10:16 +0300

    refactor: transfer the person component to the person.js file.

--- src/components/aboutTeam/aboutTeam.js
+++ src/components/aboutTeam/aboutTeam.js
@@ -1,62 +1,16 @@
 import React from 'react';
 import teamInfo from './teamInfo/teamInfo';
+import Person from './person';
 
 import './aboutTeam.scss';
 
 const AboutTeam = () => (
-  <Person teamInfo={teamInfo} />
+  <div className="about_container">
+    <p className="our_team">Our fantantastic team members</p>
+    <div className="wrapper_card">
+      {teamInfo.map((person) => (<Person personInfo={person} key={person.id} />))}
+    </div>
+  </div>
 );
 
-const Person = (team) => {
-  const personInfo = team.teamInfo.map((el) => (
-
-    <div className="cards_person" key={el.id}>
-      <div className="avatar_and_name">
-        <div className="avatar">
-          <img src={el.avatar} alt="avatar" className="img_avatar" />
-        </div>
-        <div className="full_name">
-          <p>
-            {el.name.first}
-            {el.name.sur}
-          </p>
-          <p>
-            {el.name.nick}
-          </p>
-        </div>
-      </div>
-      <div className="about_person">
-        <p>
-          {el.aboutPerson}
-        </p>
-        <div className="contribution">
-          <p>
-            contribution:
-            {' '}
-            {el.contribution}
-          </p>
-          <div className="wrapper_contribution_line">
-            <div className="contribution_line" style={{ width: el.contribution }} />
-          </div>
-        </div>
-        <div>
-          <p className="link_github">
-            <a href={el.hrefGitHub}>
-              <img src="/src/assets/image/icon/github.png" alt="gitHub link" className="github_img" />
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  ));
-
-  return (
-    <div className="about_container">
-      <p className="our_team">Our fantantastic team members</p>
-      <div className="wrapper_card">
-        { personInfo }
-      </div>
-    </div>
-  );
-};
 export default AboutTeam;
