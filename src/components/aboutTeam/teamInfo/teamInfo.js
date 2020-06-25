29e827bbcabfb60811279b263ba28462a7b584e1
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-25 17:01:01 +0300

    refactor: rename field 'hrefGitHub' to 'linkGitHub' and  delete field 'linkGithub' for mentor

--- src/components/aboutTeam/teamInfo/teamInfo.js
+++ src/components/aboutTeam/teamInfo/teamInfo.js
@@ -5,7 +5,7 @@ const teamInfo = [{
     nick: 'SeGeGha',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/21230284?s=400&u=8c419c79f7ab1e817ce05f2e1ecacc261f5aeea2&v=4',
-  hrefGitHub: 'https://github.com/segegha',
+  linkGitHub: 'https://github.com/segegha',
   aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -25,7 +25,7 @@ const teamInfo = [{
     nick: 'blacktruth',
   },
   avatar: 'https://avatars0.githubusercontent.com/u/60391325?s=400&u=a246f4fe7576ce34a6e702e1a32bd606438b8d17&v=4',
-  hrefGitHub: 'https://github.com/blacktruth',
+  linkGitHub: 'https://github.com/blacktruth',
   aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -45,7 +45,7 @@ const teamInfo = [{
     nick: 'RVitaly1978',
   },
   avatar: 'https://avatars1.githubusercontent.com/u/55198465?s=400&v=4',
-  hrefGitHub: 'https://github.com/rvitaly1978',
+  linkGitHub: 'https://github.com/rvitaly1978',
   aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -65,7 +65,7 @@ const teamInfo = [{
     nick: 'kaxaru',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/6479576?s=400&u=4510e62e237855c67bec435daff8a6d8cfbe2e4a&v=4',
-  hrefGitHub: 'https://github.com/kaxaru',
+  linkGitHub: 'https://github.com/kaxaru',
   aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -85,7 +85,7 @@ const teamInfo = [{
     nick: 'Sultonali-Ali',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/54768203?s=400&u=b84697c9df943929c4739625b1d480309146caa0&v=4',
-  hrefGitHub: 'https://github.com/sultonali-ali',
+  linkGitHub: 'https://github.com/sultonali-ali',
   aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -105,7 +105,7 @@ const teamInfo = [{
     nick: 'VitaliyTom',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/39959686?s=400&u=e24881ac77dfa6c87d99c46b77515f443a89c962&v=4',
-  hrefGitHub: 'https://github.com/VitaliyTom',
+  linkGitHub: 'https://github.com/VitaliyTom',
   aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -125,7 +125,6 @@ const teamInfo = [{
     nick: 'juliamv',
   },
   avatar: '/src/assets/image/avatar/woman.png',
-  hrefGitHub: '#',
   aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
