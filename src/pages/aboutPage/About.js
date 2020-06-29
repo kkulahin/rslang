11dd138b1ba425b67c15af940f250f3861ad8845
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-06-29 16:44:51 +0300

    refactor: move page about to /src/pages/aboutPage

--- src/pages/aboutPage/About.js
+++ src/pages/aboutPage/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AboutTeam from '../components/aboutTeam/aboutTeam';
+import AboutTeam from './aboutTeam/aboutTeam';
 
 const About = () => (
   <div className="about_page">
--- src/pages/aboutPage/aboutTeam/person.js
+++ src/pages/aboutPage/aboutTeam/person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '../button/Button';
+import Button from '../../../components/button/Button';
 
 const Person = ({ personInfo }) => {
   const {
