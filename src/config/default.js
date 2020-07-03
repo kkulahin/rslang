e9b31d483781a56fc31c34f3fb9d9190f453e61b
Sergey <s3gaddtv@gmail.com>
2020-07-03 14:02:08 +0300

    fix: remove unused setting, rename category setting, add max value for number-picker

--- src/config/default.js
+++ src/config/default.js
@@ -1,8 +1,12 @@
 const SchoolURL = 'https://afternoon-falls-25894.herokuapp.com';
+
 const cookieLifeCyrcle = 15552000;
+
 const numberPickerMinValue = 1;
+const numberPickerMaxValue = 300;
+
 const defaultInputRatio = 5;
 
 export {
-  SchoolURL, cookieLifeCyrcle, numberPickerMinValue, defaultInputRatio,
+  SchoolURL, cookieLifeCyrcle, numberPickerMinValue, numberPickerMaxValue, defaultInputRatio,
 };
