2fd9154b6d12a3e2b0b1101db5149c65bd2ca583
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-07-05 19:05:36 +0300

    feat: add left and right arrows handle, add useReducer

--- src/components/wordCard/WordCard.js
+++ src/components/wordCard/WordCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 
 import ContainerWithShadow from '../containerWithShadow/ContainerWithShadow';
@@ -7,14 +7,19 @@ import CardContent from './cardContent/CardContent';
 
 import './WordCard.scss';
 import Word from '../../utils/spacedRepetition/Word';
-// import WordQueue from '../../utils/spacedRepetition/WordQueue';
+// import WordQueue from "../../utils/spacedRepetition/WordQueue";
 
 const checkCorrect = ({ value, word }) => value.toLowerCase() === word.toLowerCase();
 
-/**
- * @param {Object} params
- * @param {WordQueue} params.wordQueue
- */
+const initialState = {
+  isWordInput: false,
+  isInputInFocus: { isFocus: true },
+  isCorrect: false,
+  isShowBtnClick: false,
+  isAgainBtnClick: false,
+  isAudioOn: { audioOn: false },
+  value: '',
+};
 
 const WordCard = ({
   // wordQueue,
@@ -32,91 +37,111 @@ const WordCard = ({
   onWordMistaken,
   hasPrevious,
 }) => {
-  const [isWordInput, setIsWordInput] = useState(false);
-  const [isInputInFocus, setIsInputInFocus] = useState({ isFocus: true });
-  const [isCorrect, setIsCorrect] = useState(false);
-  const [isShowBtnClick, setIsShowBtnClick] = useState(false);
-  const [isAgainBtnClick, setIsAgainBtnClick] = useState(false);
-  const [isAudioOn, setIsAudioOn] = useState({ audioOn: false });
-  const [value, setValue] = useState('');
-
   const { isAudioAuto, isComplexityBtn } = settings;
   const { definition: { word } } = currentWord;
 
-  const resetWord = () => {
-    setIsWordInput(false);
-    setIsCorrect(false);
-    setIsShowBtnClick(false);
-    setIsAgainBtnClick(false);
-    setIsAudioOn({ audioOn: false });
-    setIsInputInFocus({ isFocus: true });
-    setValue('');
-  };
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'resetWord':
+        return initialState;
+      case 'setState':
+        return { ...state, ...action.payload };
+      case 'handleAnswer':
+        const newState = {
+          value: '',
+          isWordInput: true,
+          isInputInFocus: { isFocus: false },
+        };
 
-  const handleAnswer = (isCorrectAnswer) => {
-    setValue('');
-    setIsWordInput(true);
-    setIsInputInFocus({ isFocus: false });
+        if (action.payload.isCorrectAnswer) {
+          newState.isCorrect = true;
+          onWordAnswered();
+        } else {
+          onWordMistaken();
+        }
 
-    if (isCorrectAnswer) {
-      setIsCorrect(true);
-      onWordAnswered();
-    } else {
-      onWordMistaken();
+        if (isAudioAuto) {
+          newState.isAudioOn = { audioOn: true };
+        }
+
+        return { ...state, ...newState };
+      default:
+        return state;
     }
+  }
 
-    if (isAudioAuto) {
-      setIsAudioOn({ audioOn: true });
-    }
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getNextWord = () => {
-    resetWord();
+    dispatch({ type: 'resetWord' });
     onNextBtnClick();
   };
 
   const handleAudioEnd = () => {
-    if ((isCorrect || isShowBtnClick) && !isComplexityBtn) {
+    if ((state.isCorrect || state.isShowBtnClick) && !isComplexityBtn) {
       getNextWord();
     }
   };
 
   const handleInputChange = (evt) => {
-    setValue(evt.target.value);
+    dispatch({ type: 'setState', payload: { value: evt.target.value } });
   };
 
   const handleNavigatePrevClick = () => {
-    resetWord();
+    dispatch({ type: 'resetWord' });
     onPrevBtnClick();
   };
 
   const handleNavigateNextClick = () => {
-    if (isCorrect || isShowBtnClick || isAnswered) {
+    if (state.isCorrect || state.isShowBtnClick || isAnswered) {
       getNextWord();
     } else {
-      handleAnswer(checkCorrect({ value, word }));
+      dispatch({
+        type: 'handleAnswer',
+        payload: {
+          isCorrect: checkCorrect({ value: state.value, word }),
+        },
+      });
     }
   };
 
   const handleInputEnter = (evt) => {
     if (evt.key === 'Enter') {
       const { value: val } = evt.target;
-      handleAnswer(checkCorrect({ value: val, word }));
+      dispatch({
+        type: 'handleAnswer',
+        payload: {
+          isCorrect: checkCorrect({ value: val, word }),
+        },
+      });
     }
   };
 
   const handleInputFocus = () => {
-    if (isWordInput) {
-      setIsWordInput(false);
-      setIsAudioOn({ audioOn: false });
+    if (state.isWordInput) {
+      dispatch({
+        type: 'setState',
+        payload: {
+          isWordInput: false,
+          isAudioOn: { audioOn: false },
+        },
+      });
     }
   };
 
   const handleShowBtnClick = () => {
-    setIsShowBtnClick(true);
-    handleAnswer(false); //----------------------------------------------
-    onWordAnswered(); //-------------------------------------------------
-    setIsCorrect(true); //-----------------------------------------------
+    dispatch({
+      type: 'setState',
+      payload: {
+        isShowBtnClick: true,
+        isCorrect: true,
+      },
+    });
+    dispatch({
+      type: 'handleAnswer',
+      payload: { isCorrect: false },
+    });
+    // onWordAnswered();
   };
 
   const handleWordComplexityBtnClick = (id) => {
@@ -124,7 +149,12 @@ const WordCard = ({
   };
 
   const handleAgainBtnClick = () => {
-    setIsAgainBtnClick(true);
+    dispatch({
+      type: 'setState',
+      payload: {
+        isAgainBtnClick: true,
+      },
+    });
     onAgainBtnClick();
   };
 
@@ -137,6 +167,27 @@ const WordCard = ({
     handlers[id]();
   };
 
+  const createHandleKeydown = (actualState) => (evt) => {
+    const { key } = evt;
+
+    if (key === 'ArrowRight') {
+      handleNavigateNextClick();
+    } else if ((key === 'ArrowLeft')
+        && hasPrevious
+        && (!actualState.isCorrect && !actualState.isShowBtnClick && !isAnswered)) {
+      handleNavigatePrevClick();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeydown = createHandleKeydown(state);
+    document.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  }, [state]);
+
   return (
     <div className="card-unit">
       <div className="card__container">
@@ -145,7 +196,7 @@ const WordCard = ({
           id="prev"
           onClick={handleNavigatePrevClick}
           isInvisible={!hasPrevious}
-          isDisabled={isCorrect || isShowBtnClick || isAnswered}
+          isDisabled={state.isCorrect || state.isShowBtnClick || isAnswered}
         />
         <ContainerWithShadow padding="20px">
           <CardContent
@@ -158,14 +209,8 @@ const WordCard = ({
             onCardBtnClick={handleCardBtnClick}
             onWordComplexityBtnClick={handleWordComplexityBtnClick}
             onAudioEnd={handleAudioEnd}
-            value={value}
-            isShowBtnClick={isShowBtnClick}
-            isAgainBtnClick={isAgainBtnClick}
-            isWordInput={isWordInput}
-            isCorrect={isCorrect}
-            isPrevWord={isAnswered}
-            isAudioOn={isAudioOn}
-            isInputInFocus={isInputInFocus}
+            isPrevWord={isAnswered} //----------------------------------
+            {...state}
           />
         </ContainerWithShadow>
         <NavigateBtn
