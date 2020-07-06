b85187ee0b47d04b9ba013e40fc2cfe632ba6051
Sergey <s3gaddtv@gmail.com>
2020-07-06 21:41:35 +0300

    feat: add responsive for login page

--- src/components/checkbox/Checkbox.js
+++ src/components/checkbox/Checkbox.js
@@ -3,10 +3,31 @@ import PropTypes from 'prop-types';
 
 import './Checkbox.scss';
 
-const Checkbox = ({ handleSwitch }) => <input type="checkbox" className="checkbox" onChange={handleSwitch} />;
+const Checkbox = ({
+  labelContent, handleSwitch, inputId, isChecked,
+}) => (
+  <label htmlFor={inputId}>
+    <input
+      id={inputId}
+      type="checkbox"
+      className="checkbox"
+      checked={isChecked}
+      onChange={handleSwitch}
+    />
+    {labelContent}
+  </label>
+);
 
 Checkbox.propTypes = {
+  labelContent: PropTypes.string,
+  inputId: PropTypes.string.isRequired,
+  isChecked: PropTypes.bool,
   handleSwitch: PropTypes.func.isRequired,
+};
+
+Checkbox.defaultProps = {
+  labelContent: '',
+  isChecked: false,
 };
 
 export default Checkbox;
