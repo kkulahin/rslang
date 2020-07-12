1de2d01633f2ec91b890e1c2f3395157a11c939a
Sergey <s3gaddtv@gmail.com>
2020-07-12 21:38:07 +0300

    fix: promo page

--- src/pages/promoPage/Promo.js
+++ src/pages/promoPage/Promo.js
@@ -4,20 +4,8 @@ import './Promo.scss';
 
 const Promo = () => (
   <div className="promo">
-
     <div className="promo__header">
       <h2 className="promo__header-title">RS Lang App education system</h2>
-      <div className="promo__video">
-        <iframe
-          title="Promo video content for RS Lang"
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/WOOpShPt7Uo"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
       <p className="promo__header-text">
         <div>
           <h2>
@@ -144,7 +132,17 @@ const Promo = () => (
         </div>
       </p>
     </div>
-
+    <div className="promo__video">
+      <iframe
+        title="Promo video content for RS Lang"
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/WOOpShPt7Uo"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
   </div>
 );
 
