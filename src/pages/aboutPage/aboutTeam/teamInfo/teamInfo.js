d45502670d959bbbab6d6df041735f34d409f6b8
vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-07-12 21:11:05 +0300

    refactor: replace placeholders: about team

--- src/pages/aboutPage/aboutTeam/teamInfo/teamInfo.js
+++ src/pages/aboutPage/aboutTeam/teamInfo/teamInfo.js
@@ -2,141 +2,116 @@ const teamInfo = [Object.freeze({
   name: {
     first: 'Sergey',
     sur: ' ',
-    nick: 'SeGeGha',
+    nick: 'Developer',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/21230284?s=400&u=8c419c79f7ab1e817ce05f2e1ecacc261f5aeea2&v=4',
   linkGitHub: 'https://github.com/segegha',
-  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, 
-  remaining essentially unchanged.`,
+  aboutPerson: `JavaScript && CSS: Projects from courses - Implementation of the functionality of a one-page sites, 
+  2018 year.
+  Delphi: Student project - Automatic calculations of economic indicators, 2016 year.  
+  Coding on FreeCodeCamp platform. Courses on htmlacademy.`,
   id: 1,
-  contribution: `Where can I get some? 
-  There are many variations of passages of Lorem Ipsum available, 
-  but the majority have suffered alteration in some form, by injected humour, 
-  or randomised words which don't look even slightly believable. 
-  If you are going to use a passage of Lorem Ipsum, 
-  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
+  contribution: `Create: switcher component, sheckbox component, greeting component,
+  continue training block, number-picker component, greeting wrapper component, settings page, sprint game.
+  Bind 2 inputs (number-pickers from setting page) together
+  Add loading and saving user settings from-to server.
+   `,
 }), Object.freeze({
   name: {
     first: 'Kanstantsin',
     sur: ' Kulahin',
-    nick: 'blacktruth',
+    nick: 'Team Lead',
   },
   avatar: 'https://avatars0.githubusercontent.com/u/60391325?s=400&u=a246f4fe7576ce34a6e702e1a32bd606438b8d17&v=4',
   linkGitHub: 'https://github.com/blacktruth',
-  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, 
-  remaining essentially unchanged. `,
+  aboutPerson: `Web developer with 4 year 
+  of overall IT experience.`,
   id: 2,
-  contribution: `Where can I get some? 
-  There are many variations of passages of Lorem Ipsum available, 
-  but the majority have suffered alteration in some form, by injected humour, 
-  or randomised words which don't look even slightly believable. 
-  If you are going to use a passage of Lorem Ipsum, 
-  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
+  contribution: `Netlify continuous deployment configuration, 
+  webhook configuration from Netlify & Github to Discord channels.
+  Create design in FIgma. Create TabbedFolder component. 
+  Investigate and describe spaced repetition algorithm.
+  Create WordController, WordModel, statistics query, WordQueue to use in the game, connect word queue to the card.
+  Connect Statistics backend to Statistics pages & Greeting page.
+  Connect Statistics & Word backend to Main card page.`,
 }), Object.freeze({
   name: {
     first: 'Vitaly',
     sur: ' Raicheu',
-    nick: 'RVitaly1978',
+    nick: 'Developer',
   },
   avatar: 'https://avatars1.githubusercontent.com/u/55198465?s=400&v=4',
   linkGitHub: 'https://github.com/rvitaly1978',
-  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, 
-  remaining essentially unchanged.`,
+  aboutPerson: `It is very important for me to do interesting things, improve my knowledge and be a part of a team.
+  My programming skills are quite modest. 
+  But to do my current job, I had to learn a lot of software stuff like SCAD Office, Autocad, MS Office, 
+  and many others, so my learning skills are on a good level i think.`,
   id: 3,
-  contribution: `Where can I get some? 
-  There are many variations of passages of Lorem Ipsum available, 
-  but the majority have suffered alteration in some form, by injected humour, 
-  or randomised words which don't look even slightly believable. 
-  If you are going to use a passage of Lorem Ipsum, 
-  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
+  contribution: `Create: reusable input component, linear progress bar, round progress bar, word card. 
+  Add responsive word card.`,
 }), Object.freeze({
   name: {
     first: 'Kaxaru',
     sur: ' ',
-    nick: 'kaxaru',
+    nick: 'Technical Lead',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/6479576?s=400&u=4510e62e237855c67bec435daff8a6d8cfbe2e4a&v=4',
   linkGitHub: 'https://github.com/kaxaru',
-  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, 
-  remaining essentially unchanged.`,
+  aboutPerson: `I started training skills after university. At that moment i worked with 1c on my work, 
+  but parallel I studied .Net. This programming language adviced my friend. It’s primary reason why i picked to study it. 
+  Furthermore in my city epam conduct courses like a .Net and java. Like so it’s became my fundamental programming language. 
+  However I guess what it’s so few for me, because I always had an interest in frontend. 
+  I started to study it with first group in loftblog. `,
   id: 4,
-  contribution: `Where can I get some? 
-  There are many variations of passages of Lorem Ipsum available, 
-  but the majority have suffered alteration in some form, by injected humour, 
-  or randomised words which don't look even slightly believable. 
-  If you are going to use a passage of Lorem Ipsum, 
-  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
+  contribution: `Create: signup page, signin page, header component,
+  added games page and simple controller, begin speakIt game controller.
+  Add "speak it","savanna" mini-game.
+  Add game levels "savanna".
+  Add game levels "speakIt".
+  Setup routing.`,
 }), Object.freeze({
   name: {
     first: 'Ali',
     sur: ' ',
-    nick: 'Sultonali-Ali',
+    nick: 'Developer',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/54768203?s=400&u=b84697c9df943929c4739625b1d480309146caa0&v=4',
   linkGitHub: 'https://github.com/sultonali-ali',
-  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, 
-  remaining essentially unchanged.`,
+  aboutPerson: `
+  The main goal is to get the most knowledge regardless of the final result. 
+  This is a challenge for me because I have never touched this direction. 
+  I’m here to test myself and my fortitude.Responsible, logical thinking, hardworking and able to make decisions.
+  Have a passion for a career in developing. Good motivated and keen to learn.`,
   id: 5,
-  contribution: `Where can I get some? 
-  There are many variations of passages of Lorem Ipsum available, 
-  but the majority have suffered alteration in some form, by injected humour, 
-  or randomised words which don't look even slightly believable. 
-  If you are going to use a passage of Lorem Ipsum, 
-  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
+  contribution: `Create: reusable button component, radio button component,
+  dictionary basic layout, promo page, statistic page, audiocall game`,
 }), {
   name: {
     first: 'Vitaliy',
     sur: ' Tomashevich',
-    nick: 'VitaliyTom',
+    nick: 'Developer',
   },
   avatar: 'https://avatars2.githubusercontent.com/u/39959686?s=400&u=e24881ac77dfa6c87d99c46b77515f443a89c962&v=4',
   linkGitHub: 'https://github.com/VitaliyTom',
-  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, 
-  remaining essentially unchanged.`,
+  aboutPerson: `I am very interested in web development. At the moment, mastering the back-end and the front-end. 
+  Further, the goal is to become one of the best full-stack developers.Good administration and organisational skills.
+  Having a logical approach to problem solving.
+  Responsible and with strong attention to detail.`,
   id: 6,
-  contribution: `Where can I get some? 
-  There are many variations of passages of Lorem Ipsum available, 
-  but the majority have suffered alteration in some form, by injected humour, 
-  or randomised words which don't look even slightly believable. 
-  If you are going to use a passage of Lorem Ipsum, 
-  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
+  contribution: `Create: aboutTeam component, person component, team info,
+  notification component, home page spinner, progress bar for home page`,
 }, Object.freeze({
   name: {
     first: 'julia',
     sur: ' ',
-    nick: 'juliamv',
+    nick: 'Mentor',
   },
   avatar: '',
-  aboutPerson: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, 
-  remaining essentially unchanged.`,
+  aboutPerson: `Programming makes it possible to express themselves and create products for millions users. 
+  I like to see the result on the screen, so I chose the frontend.`,
   id: 7,
-  contribution: `Where can I get some? 
-  There are many variations of passages of Lorem Ipsum available, 
-  but the majority have suffered alteration in some form, by injected humour, 
-  or randomised words which don't look even slightly believable. 
-  If you are going to use a passage of Lorem Ipsum, 
-  you need to be sure there isn't anything embarrassing hidden in the middle of text. `,
+  contribution: `Assistance in resolving emerging difficulties and relevant instructions in the development process. 
+  Code review`,
 })];
 
 export default teamInfo;
